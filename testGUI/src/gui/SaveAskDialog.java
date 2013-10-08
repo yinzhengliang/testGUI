53775e7b2c4972ed84c71fda55138ba63bc33edc
Zhengliang Yin <yinzhengliang@tamu.edu>
2013-10-08 14:03:53 -0500

    gui for generating and modifying test cases
    to make this run:
    1. you need to add strontium project, i don't know why just adding the
    sr-core and sr-recognition doesn't work. The program yells that "The
    type org.codehaus.jackson.annotate.JsonTypeInfo$Id cannot be resolved.
    It is indirectly referenced from required .class files"
    
    2. you need to have openawt.jar openawt-1.0.jar and openawt-1.0.src.jar

---
+++ testGUI/src/gui/SaveAskDialog.java
@@ -0,0 +1,157 @@
+/*******************************************************************************
+ *  Revision History:<br>
+ *  SRL Member - File created
+ *
+ *  <p>
+ *  <pre>
+ *  This work is released under the BSD License:
+ *  (C) 2012 Sketch Recognition Lab, Texas A&M University (hereafter SRL @ TAMU)
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *      * Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *      * Neither the name of the Sketch Recognition Lab, Texas A&M University 
+ *        nor the names of its contributors may be used to endorse or promote 
+ *        products derived from this software without specific prior written 
+ *        permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY SRL @ TAMU ``AS IS'' AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL SRL @ TAMU BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  </pre>
+ *  
+ *******************************************************************************/
+package gui;
+
+import java.awt.AWTKeyStroke;
+import java.awt.Component;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.util.HashSet;
+
+import javax.swing.Icon;
+import javax.swing.JComponent;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
+
+public class SaveAskDialog {
+
+	/**
+	 * Global variables
+	 */
+	private boolean m_isKeyPressed;
+	private Object m_selectedValue;
+
+	public SaveAskDialog() {
+		m_isKeyPressed = false;
+		m_selectedValue = null;
+	}
+
+	/**
+	 * Show Option Dialog replaces JOptionPane.showConfirmDialog so that arrow
+	 * keys can be used to select buttons in the dialog
+	 */
+	public int showOptionDialog(Component parentComponent, Object message,
+			String title, int optionType, int messageType, Icon icon,
+			Object[] options, Object initialValue) {
+
+		// the JOptionPane to modify
+		JOptionPane optionPane = new JOptionPane(message, messageType,
+				optionType, icon, options, initialValue);
+
+		optionPane.setInitialValue(initialValue);
+
+		HashSet<AWTKeyStroke> forwardSet = new HashSet<AWTKeyStroke>();
+		forwardSet
+				.add(AWTKeyStroke.getAWTKeyStroke(KeyEvent.VK_RIGHT, 0, true));
+		forwardSet.add(AWTKeyStroke.getAWTKeyStroke(KeyEvent.VK_UP, 0, true));
+		forwardSet.add(AWTKeyStroke.getAWTKeyStroke(KeyEvent.VK_TAB, 0, true));
+
+		optionPane.setFocusTraversalKeys(
+				KeyboardFocusManager.FORWARD_TRAVERSAL_KEYS, forwardSet);
+
+		HashSet<AWTKeyStroke> backwardSet = new HashSet<AWTKeyStroke>();
+		backwardSet.add(AWTKeyStroke.getAWTKeyStroke(KeyEvent.VK_TAB,
+				InputEvent.SHIFT_DOWN_MASK, true));
+		backwardSet
+				.add(AWTKeyStroke.getAWTKeyStroke(KeyEvent.VK_LEFT, 0, true));
+		backwardSet
+				.add(AWTKeyStroke.getAWTKeyStroke(KeyEvent.VK_DOWN, 0, true));
+
+		optionPane.setFocusTraversalKeys(
+				KeyboardFocusManager.BACKWARD_TRAVERSAL_KEYS, backwardSet);
+
+		final JDialog dialog = optionPane.createDialog(parentComponent, title);
+
+		ActionListener yesActionListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				m_isKeyPressed = true;
+				dialog.dispose();
+				m_selectedValue = new Integer(0);
+			}
+		};
+
+		KeyStroke yKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_Y, 0, true);
+		optionPane.registerKeyboardAction(yesActionListener, yKeyStroke,
+				JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+		ActionListener noActionListener = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				m_isKeyPressed = true;
+				dialog.dispose();
+				m_selectedValue = new Integer(1);
+			}
+		};
+
+		KeyStroke nKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_N, 0, true);
+		optionPane.registerKeyboardAction(noActionListener, nKeyStroke,
+				JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+		optionPane.selectInitialValue();
+
+		dialog.setVisible(true);
+
+		if (!m_isKeyPressed) {
+			dialog.dispose();
+			m_selectedValue = optionPane.getValue();
+
+			m_isKeyPressed = false;
+		} else {
+			m_isKeyPressed = false;
+			return ((Integer) m_selectedValue).intValue();
+		}
+
+		if (m_selectedValue == null) {
+			return JOptionPane.CLOSED_OPTION;
+		}
+
+		if (options == null) {
+			if (m_selectedValue instanceof Integer) {
+				return ((Integer) m_selectedValue).intValue();
+			}
+			return JOptionPane.CLOSED_OPTION;
+		}
+
+		for (int counter = 0, maxCounter = options.length; counter < maxCounter; counter++) {
+			if (options[counter].equals(m_selectedValue)) {
+				return counter;
+			}
+		}
+		return JOptionPane.CLOSED_OPTION;
+	}
+}
---
+++ testGUI/src/gui/SketchCanvas.java
@@ -0,0 +1,148 @@
+/*******************************************************************************
+ *  Revision History:<br>
+ *  SRL Member - File created
+ *
+ *  <p>
+ *  <pre>
+ *  This work is released under the BSD License:
+ *  (C) 2012 Sketch Recognition Lab, Texas A&M University (hereafter SRL @ TAMU)
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *      * Redistributions of source code must retain the above copyright
+ *        notice, this list of conditions and the following disclaimer.
+ *      * Redistributions in binary form must reproduce the above copyright
+ *        notice, this list of conditions and the following disclaimer in the
+ *        documentation and/or other materials provided with the distribution.
+ *      * Neither the name of the Sketch Recognition Lab, Texas A&M University 
+ *        nor the names of its contributors may be used to endorse or promote 
+ *        products derived from this software without specific prior written 
+ *        permission.
+ *  
+ *  THIS SOFTWARE IS PROVIDED BY SRL @ TAMU ``AS IS'' AND ANY
+ *  EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ *  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *  DISCLAIMED. IN NO EVENT SHALL SRL @ TAMU BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ *  ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ *  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *  </pre>
+ *  
+ *******************************************************************************/
+package gui;
+
+import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JLayeredPane;
+import javax.swing.OverlayLayout;
+
+import srl.core.sketch.Sketch;
+import srl.core.sketch.controllers.SketchController;
+
+
+public class SketchCanvas extends JLayeredPane implements MouseListener {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8757954344808440542L;
+
+	public static final int SKETCH = 1;
+	public static final int BACKGROUND = 2;
+	public static final int EVERYTHING = SKETCH | BACKGROUND;
+
+	protected BackgroundImagePanel background;
+	protected SketchPanel drawPanel;
+
+	public SketchCanvas() {
+		this(new Sketch());
+	}
+	public SketchCanvas(Sketch sketch){
+		this(new SketchController(sketch));
+	}
+	public SketchCanvas(SketchController controller){
+
+		setLayout(new OverlayLayout(this));
+
+		background = new BackgroundImagePanel();
+		add(background, 0);
+
+		drawPanel = new SketchPanel(controller);
+		drawPanel.setOpaque(false);
+		add(drawPanel, 0);
+
+		drawPanel.addMouseListener(this);
+	}
+	public void setSketch(Sketch s) {
+		drawPanel.setSketch(s);
+	}
+
+	public Sketch getSketch() {
+		return drawPanel.getSketch();
+	}
+	public SketchController getSketchController(){
+		return drawPanel.getSketchController();
+	}
+
+	@Override
+	public void setPreferredSize(Dimension d) {
+		super.setPreferredSize(d);
+		drawPanel.setPreferredSize(d);
+		background.setPreferredSize(d);
+	}
+
+	public void setStrokeWidth(float width) {
+		drawPanel.setStrokeWidth(width);
+	}
+
+	public void setStrokeColor(org.openawt.Color color) {
+		drawPanel.setStrokeColor(color);
+	}
+
+	public void clear() {
+		clear(EVERYTHING);
+	}
+
+	public void clear(int howMuch) {
+		if ((howMuch & BACKGROUND) != 0) {
+			background.clear();
+		}
+
+		if ((howMuch & SKETCH) != 0) {
+			drawPanel.clear();
+		}
+		repaint();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		repaint();
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		clear(BACKGROUND);
+		repaint();
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
+	}
+}
